cc2c057e42a403dca9f52c3cd526e6fb2de42ea8
Ezakii-Y <3197404862@qq.com>
2025-03-15 23:48:44 +0800

    some preparations for subsequent learning

---
+++ start/0-2.cpp
@@ -0,0 +1,7 @@
+# include <iostream>
+
+int main() {
+    std::cout<<"This (\") is a quote, and this (\\) is a backslash."<<std::endl;
+    std::cout<<"\t34 \t56 \n \t33 \t38"<<std::endl;
+    return 0;
+}
